185038f1f50f71438c7e8e0200c9ee8a0ba4e682
Samuel Sylvester <sasy260902@gmail.com>
2024-08-14 01:06:52 +0100

    Update 8-all.py

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -7,12 +7,12 @@ in a collection
 
 
 def list_all(mongo_collection):
-  doc_list = []
-  """
-  Return an empty list
-  if no document in the collection
-  """
-  for doc in mongo_collection.find():
-    if doc:
-      doc_list.append(doc)
-  return doc_list
+    doc_list = []
+    """
+    Return an empty list
+    if no document in the collection
+    """
+    for doc in mongo_collection.find():
+        if doc:
+            doc_list.append(doc)
+    return doc_list
