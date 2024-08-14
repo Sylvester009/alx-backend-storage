7f54bc27710c4af82f7e9a4ba2d5b5904f5d1ed4
Samuel Sylvester <sasy260902@gmail.com>
2024-08-14 01:03:36 +0100

    list  all documents in database- task 8

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""
+Python function that lists all documents
+in a collection
+"""
+
+
+def list_all(mongo_collection):
+  doc_list = []
+  """
+  Return an empty list
+  if no document in the collection
+  """
+  for doc in mongo_collection.find():
+    if doc:
+      doc_list.append(doc)
+  return doc_list
